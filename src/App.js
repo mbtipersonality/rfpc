a9fbe3ed9a7c477fecee3dcbed70ccfc8c51e0d4
ramdev8877771 <156853367+ramdev8877771@users.noreply.github.com>
2024-04-17 19:34:35 +0530

    Update App.js
    Changed link to yewtu.be

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import search from './search'
 function App() {
   const handleClick = () => {
     const randSearch = search[Math.floor(Math.random() * 2997 )]
-
-    window.location = `https://www.google.com/search?q=site%3Amayoclinic.org+${randSearch}&sca_esv=8db531018b40ad46&sca_upv=1&ei=WEYXZq-aCLbbseMPq828wAw&ved=0ahUKEwivpvmoirmFAxW2bWwGHasmD8gQ4dUDCBA&uact=5&oq=site%3Amayoclinic.org+a&gs_lp=Egxnd3Mtd2l6LXNlcnAiFXNpdGU6bWF5b2NsaW5pYy5vcmcgYUiUCFCCBliCBnACeACQAQCYAYQCoAGEAqoBAzItMbgBA8gBAPgBAZgCAKACAJgDAIgGAZIHAKAHLQ&sclient=gws-wiz-serp`
+    
+    window.location = `https://yewtu.be/search?q=${randSearch}`
   }
   return (
     <div className='App' onClick={handleClick}>
