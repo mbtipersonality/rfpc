e8e247625cc8124a82da3d7fd997a2a7b2ae9216
ramdev8877771 <156853367+ramdev8877771@users.noreply.github.com>
2024-04-23 15:51:23 +0530

    Update App.js
    Changed url to remedyskin

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const handleClick = () => {
     const randSearch = search[Math.floor(Math.random() * 2997 )]
     
-    window.location = `https://yewtu.be/search?q=${randSearch}`
+    window.location = `https://www.google.com/search?q=site%3Aremedyskin.com%2Fblogs+${randSearch}&sca_esv=f9e9aef3ae3c6c42&sca_upv=1&ei=34onZuaQKNbV4-EP_N6B6Ak&oq=site%3Aremedyskin.com%2Fblogs+a&gs_lp=EhNtb2JpbGUtZ3dzLXdpei1zZXJwIhtzaXRlOnJlbWVkeXNraW4uY29tL2Jsb2dzIGFIiwZQAFgAcAB4AJABAJgBggKgAYICqgEDMi0xuAEDyAEAmAIAoAIAmAMAiAYBkgcAoAct&sclient=mobile-gws-wiz-serp`
   }
   return (
     <div className='App' onClick={handleClick}>
