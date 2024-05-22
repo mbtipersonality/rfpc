ce8fdfc3764d6fa357bed7338b2492d250a24d54
mbtipersonality <156853367+mbtipersonality@users.noreply.github.com>
2024-05-22 09:50:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css'
 import React from 'react'
-import search from './search'
 
 function App() {
   const handleClick = () => {
-    const randSearch = search[Math.floor(Math.random() * 2997 )]
     
-    window.location = `https://www.google.com/search?q=site%3Aremedyskin.com%2Fblogs+${randSearch}&sca_esv=f9e9aef3ae3c6c42&sca_upv=1&ei=34onZuaQKNbV4-EP_N6B6Ak&oq=site%3Aremedyskin.com%2Fblogs+a&gs_lp=EhNtb2JpbGUtZ3dzLXdpei1zZXJwIhtzaXRlOnJlbWVkeXNraW4uY29tL2Jsb2dzIGFIiwZQAFgAcAB4AJABAJgBggKgAYICqgEDMi0xuAEDyAEAmAIAoAIAmAMAiAYBkgcAoAct&sclient=mobile-gws-wiz-serp`
+    const number = Math.floor(Math.random() * (500 - 11 + 1) + 11);
+    
+    window.location = `https://redditfavorites.com/product_categories?page=${number}`
   }
   return (
     <div className='App' onClick={handleClick}>
