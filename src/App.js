dae9129d252af84fee095868add599c64a24bec1
mbtipersonality <156853367+mbtipersonality@users.noreply.github.com>
2025-10-02 02:04:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import React from 'react'
 
 function App() {
   const handleClick = () => {
+
+    const search1 = Array.from({ length: 201 }, (_, i) => i + 181);
+
+    const search2 = JSON.parse(localStorage.getItem('numbers'))
+    if(!search2){
+      localStorage.setItem('numbers', JSON.stringify(search1))
+    }
+    const randSearch = search2[Math.floor(Math.random() * (search2.length - 1 + 1) + 1 )]
+
+    const newNumbers = search2.filter(item => item !== randSearch)
+    localStorage.setItem('numbers', JSON.stringify(newNumbers))
     
-    const number = Math.floor(Math.random() * (381 - 181 + 1)) + 181;
-    
-    window.location = `https://redditfavorites.com/product_categories/subjects?page=${number}`
+    window.location = `https://redditfavorites.com/product_categories/subjects?page=${randSearch}`
   }
   return (
     <div className='App' onClick={handleClick}>
